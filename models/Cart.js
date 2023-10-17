9dfbcf494b2231d9b5bd114868868a3ddb6baccf
camiloreyes1 <jreye236@fiu.edu>
2023-10-17 13:46:37 -0400

    prod and cart model commit

--- models/Cart.js
+++ models/Cart.js
@@ -8,5 +8,24 @@ const cartSchema = new Schema(
             default : 0
         },
 
+        tax: {
+            type: Number,
+            default: 1.08
+        },
+
+        total: {
+            type: Number,
+            default: 0
+        },
+
+        owner: {type: Schema.Types.ObjectId, ref: 'User'},
+        timeLeft: Date,
+        expireAt: { type: Date, expires: '5m', default: Date.now}
+    },
+
+    {
+        timestamps: true
     }
 )
+
+module.exports = model('Cart', cartSchema)
